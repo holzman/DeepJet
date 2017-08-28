55d26fc45112e2ac3e913d9830d16eb5bb878eca
Javier Duarte <jduarte1@fnal.gov>
2017-08-28 09:52:03 -0700

    add simple format

--- modules/TrainData_deepDoubleB_Test.py
+++ modules/TrainData_deepDoubleB_Test.py
@@ -205,6 +205,133 @@ class TrainData_deepDoubleB_init(TrainData_deepDoubleB):
         self.y=[alltruth]
 	print("self:")
 	print(self.y)
+
+    
+        
+class TrainData_deepDoubleB_simple(TrainData_deepDoubleB):
+    
+    def __init__(self):
+        '''
+        This is an example data format description for FatJet studies
+        '''
+        TrainData_deepDoubleB.__init__(self)
+        
+        #example of how to register global branches
+        self.addBranches(['fj_pt',
+                          'fj_eta',
+                          'fj_sdmass',
+                          'fj_n_sdsubjets',
+                          'fj_doubleb',
+                          'fj_tau21',
+                          'fj_tau32',
+                          'npv',
+                          'npfcands',
+                          'ntracks',
+                          'nsv'
+                      ])
+        
+        self.addBranches(['fj_jetNTracks',
+                          'fj_nSV',
+                          'fj_tau0_trackEtaRel_0',
+                          'fj_tau0_trackEtaRel_1',
+                          'fj_tau0_trackEtaRel_2',
+                          'fj_tau1_trackEtaRel_0',
+                          'fj_tau1_trackEtaRel_1',
+                          'fj_tau1_trackEtaRel_2',
+                          'fj_tau_flightDistance2dSig_0',
+                          'fj_tau_flightDistance2dSig_1',
+                          'fj_tau_vertexDeltaR_0',
+                          'fj_tau_vertexEnergyRatio_0',
+                          'fj_tau_vertexEnergyRatio_1',
+                          'fj_tau_vertexMass_0',
+                          'fj_tau_vertexMass_1',
+                          'fj_trackSip2dSigAboveBottom_0',
+                          'fj_trackSip2dSigAboveBottom_1',
+                          'fj_trackSip2dSigAboveCharm_0',
+                          'fj_trackSipdSig_0',
+                          'fj_trackSipdSig_0_0',
+                          'fj_trackSipdSig_0_1',
+                          'fj_trackSipdSig_1',
+                          'fj_trackSipdSig_1_0',
+                          'fj_trackSipdSig_1_1',
+                          'fj_trackSipdSig_2',
+                          'fj_trackSipdSig_3',
+                          'fj_z_ratio',
+                          ]) 
+
+        #branches that are used directly in the following function 'readFromRootFile'
+        #this is a technical trick to speed up the conversion
+        #self.registerBranches(['Cpfcan_erel','Cpfcan_eta','Cpfcan_phi',
+        #                       'Npfcan_erel','Npfcan_eta','Npfcan_phi',
+        #                       'nCpfcand','nNpfcand',
+        #                       'jet_eta','jet_phi'])
+        
+        
+    #this function describes how the branches are converted
+    def readFromRootFile(self,filename,TupleMeanStd, weighter):
+        
+        #the first part is standard, no changes needed
+        from preprocessing import MeanNormApply, MeanNormZeroPad, MeanNormZeroPadParticles, ZeroPadParticles
+        import numpy
+        import ROOT
+        
+        fileTimeOut(filename,120) #give eos 2 minutes to recover
+        rfile = ROOT.TFile(filename)
+        tree = rfile.Get("deepntuplizer/tree")
+        self.nsamples=tree.GetEntries()
+    
+        x_glb  = ZeroPadParticles(filename,TupleMeanStd,
+                                          self.branches[0],
+                                          self.branchcutoffs[0],self.nsamples)
+
+        x_db  = MeanNormZeroPadParticles(filename,TupleMeanStd,
+                                         self.branches[1],
+                                         self.branchcutoffs[1],self.nsamples)
+        
+        # now, some jets are removed to avoid pt and eta biases
+        
+        Tuple = self.readTreeFromRootToTuple(filename)
+        if self.remove:
+            # jets are removed until the shapes in eta and pt are the same as
+            # the truth class 'fj_isLight'
+            notremoves=weighter.createNotRemoveIndices(Tuple)
+            #undef=Tuple[self.undefTruth]
+            #notremoves-=undef
+        
+        if self.weight:
+            weights=weighter.getJetWeights(Tuple)
+        elif self.remove:
+            weights=notremoves
+        else:
+            print('neither remove nor weight')
+            weights=numpy.empty(self.nsamples)
+            weights.fill(1.)
+            
+            
+        # create all collections:
+        truthtuple =  Tuple[self.truthclasses]
+        alltruth=self.reduceTruth(truthtuple)
+	print(alltruth)
+        
+        # remove the entries to get same jet shapes
+        if self.remove:
+            print('remove')
+            weights=weights[notremoves > 0]
+            x_glb=x_glb[notremoves > 0]
+            x_db=x_db[notremoves > 0]
+            alltruth=alltruth[notremoves > 0]  
+        
+        newnsamp=x_glb.shape[0]
+        print('reduced content to ', int(float(newnsamp)/float(self.nsamples)*100),'%')
+        self.nsamples = newnsamp
+        
+        # fill everything
+        self.w=[weights]
+        self.x=[x_db]
+        self.z=[x_glb]
+        self.y=[alltruth]
+	print("self:")
+	print(self.y)
         
     
 class TrainData_deepDoubleB_full(TrainData_deepDoubleB):
