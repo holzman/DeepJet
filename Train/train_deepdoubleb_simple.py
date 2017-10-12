4497c90eb8f806356fc7a02a31a982fd83d8f1a3
EC2 Default User <ec2-user@ip-172-31-2-93.us-west-2.compute.internal>
2017-10-12 17:26:44 +0000

    for training on AWS

---
+++ Train/train_deepdoubleb_simple.py
@@ -0,0 +1,60 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import sys
+import os
+import keras
+#keras.backend.set_image_data_format('channels_last')
+
+
+# In[2]:
+
+class MyClass:
+    """A simple example class"""
+    def __init__(self):
+        self.inputDataCollection = ''
+        self.outputDir = ''
+
+
+# In[5]:
+import setGPU
+#os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+from training_base import training_base
+from Losses import loss_NLL
+import sys
+
+args = MyClass()
+args.inputDataCollection = '/data/shared/BumbleB/convert_20170717_ak8_deepDoubleB_simple_train_val/dataCollection.dc'
+args.outputDir = 'train_deep_simple_64_32_32_b8192'
+
+#also does all the parsing
+train=training_base(testrun=False,args=args)
+
+
+if not train.modelSet():
+    from DeepJet_models_ResNet import deep_model_doubleb
+    from models.dense import dense_model
+
+    train.setModel(deep_model_doubleb)
+    
+    train.compileModel(learningrate=0.0001,
+                       loss=['categorical_crossentropy'],
+                       metrics=['accuracy'])
+    
+
+model,history,callbacks = train.trainModel(nepochs=200, 
+                                batchsize=32, 
+                                 stop_patience=1000, 
+                                 lr_factor=0.5, 
+                                 lr_patience=10, 
+                                 lr_epsilon=0.0000001, 
+                                 lr_cooldown=2, 
+                                 lr_minimum=0.0000001, 
+                                           maxqsize=1000)
+
+
+
+
--- environment/gpu_env.sh
+++ environment/gpu_env.sh
@@ -3,7 +3,7 @@
 source activate deepjetLinux3_gpu
 
 export PYTHONPATH=`pwd`/../modules:$PYTHONPATH
-export LD_LIBRARY_PATH=`pwd`/../modules:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/../modules:/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
 export PATH=`pwd`/../scripts:$PATH
 export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
 
